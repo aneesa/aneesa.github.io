fb5d95d4b82a262b08ef33fe82b6e5ba52fd61a4
Aneesa <a_aneesa@yahoo.com>
2017-10-22 21:18:44 +0800

    Add contact icons in the header bar; Remove email form

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,7 @@
         var nav = $($anchor.attr('href'));
         if (nav.length) {
           $('html, body').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top
+              scrollTop: ($($anchor.attr('href')).offset().top)-50
           }, 1500, 'easeInOutExpo');
 
           event.preventDefault();
@@ -24,7 +24,7 @@
     });
 
     // Home section scroll
-    $(".navbar-header a").on('click', function(event) {
+    $('.navbar-header a img').on('click', function(event) {
         event.preventDefault();
         $('.navbar-nav li').removeClass('active');
         $('.navbar-nav li:first').addClass('active');
@@ -34,22 +34,22 @@
     });
 
     // Foo Bar section scroll
-    $(".overlay-detail a").on('click', function(event) {
+    $('.overlay-detail a').on('click', function(event) {
         event.preventDefault();
         $('.navbar-nav li').removeClass('active');
         $('.navbar-nav li:nth-child(2)').addClass('active');
         var hash = this.hash;
         $('html, body').animate({
-            scrollTop: $(hash).offset().top
+            scrollTop: ($(hash).offset().top)-50
         }, 900);
     });
 
     //jQuery to collapse the navbar on scroll
     $(window).scroll(function() {
-        if ($(".navbar-default").offset().top > 50) {
-            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        if ($('.navbar-default').offset().top > 50) {
+            $('.navbar-fixed-top').addClass('top-nav-collapse');
         } else {
-            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+            $('.navbar-fixed-top').removeClass('top-nav-collapse');
         }
     });
 
